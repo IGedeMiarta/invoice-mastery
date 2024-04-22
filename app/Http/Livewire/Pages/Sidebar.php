84925126324887356d060bf7f861bfed7479de25
I Gede Miarta Yasa <miartayasa10@gmail.com>
2024-04-22 23:01:34 +0800

    first commit

---
+++ app/Http/Livewire/Pages/Sidebar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Pages;
+
+use Livewire\Component;
+
+class Sidebar extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.sidebar');
+    }
+}
